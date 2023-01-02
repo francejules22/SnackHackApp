dd39bdb78719e437630a5289c4d8dc222195fce5
Francis Jules <francisjulescelesteespartero@gmail.com>
2023-01-02 17:34:00 +0800

    Project Added

---
+++ snackHack.js
@@ -0,0 +1,14 @@
+
+//Scroll Reveal
+const scroll = ScrollReveal ({
+    origin: 'top',
+    distance: '40px',
+    duration: 2000,
+    reset: true
+});
+
+
+scroll.reveal('.hero-content, .hero-img, .about-img, .about-content, .cta-section, .product-heading, .services-heading, .service-content, .footer-link-wrapper, .reference-link-wrap',{interval:100})
+
+
+
